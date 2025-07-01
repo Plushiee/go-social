e28875c8981ef12106964dc369ca8513ae6c6508
unknown <barasatyaradi@gmail.com>
2025-07-01 18:14:13 +0700

    second update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/lpernett/godotenv"
+	"github.com/sikozonpc/social/internal/db"
 	"github.com/sikozonpc/social/internal/env"
 	"github.com/sikozonpc/social/internal/env/store"
 )
@@ -12,8 +13,28 @@ func main() {
 	godotenv.Load()
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgress://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenconns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_Idle_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenconns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic("Failed to connect to the database:", err)
+	}
+
+	defer db.Close()
+	log.Println("Connected to the database successfully")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
--- internal/env/store/posts.go
+++ internal/env/store/posts.go
@@ -17,23 +17,31 @@ type Post struct {
 	UpdatedAt string   `json:"updated_at"`
 }
 
-type PostsStore struct {
+type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostsStore) Create(ctx context.Context, post *Post) error {
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	query := `
 		INSERT INTO posts (content, title, user_id, tags)
 		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
 	`
-	s.db.QueryContext(
+	err := s.db.QueryRowContext(
 		ctx,
 		query,
 		post.Content,
 		post.Title,
 		post.UserId,
 		pq.Array(post.Tags),
-	).Scan{
-		
+	).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+
+	if err != nil {
+		return err
 	}
+
+	return nil
 }
